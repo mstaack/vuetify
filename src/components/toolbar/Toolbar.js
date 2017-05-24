f6a1972ada44b11bce75ee9d75eb3afed8349118
John Leider <john.j.leider@gmail.com>
2017-05-23 21:19:47 -0400

    added initial functionality for tools scroll:

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,21 +1,64 @@
 export default {
-  functional: true,
+  name: 'toolbar',
+
+  data: () => ({
+    marginTop: 0
+  }),
 
   props: {
-    fixed: Boolean
+    fixed: Boolean,
+    tools: Boolean
   },
 
-  render (h, { data, children, props }) {
-    data.staticClass = data.staticClass ? `toolbar ${data.staticClass}` : 'toolbar'
-    if (props.fixed) data.staticClass += ' toolbar--fixed'
+  computed: {
+    styles () {
+      return {
+        marginTop: `${this.marginTop}px`
+      }
+    }
+  },
+
+  watch: {
+    tools () {
+      this.onScroll()
+    }
+  },
+
+  mounted () {
+    window.addEventListener('scroll', this.onScroll, { passive: true })
+    this.onScroll()
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll, { passive: true })
+  },
+
+  methods: {
+    onScroll () {
+      if (!this.tools) return
+
+      const marginTop = 56 - window.pageYOffset
+      this.marginTop = marginTop >= 0 ? marginTop : 0
+    }
+  },
+
+  render (h) {
+    const data = {
+      'class': {
+        'toolbar': true,
+        'toolbar--fixed': this.fixed,
+        'toolbar--tools': this.tools
+      }
+    }
 
     const pad = h('div', {
       'class': 'toolbar__pad'
-    })
+    }, [this.$slots.icon])
 
     const tools = h('div', {
-      'class': 'toolbar__tools'
-    }, children)
+      'class': 'toolbar__tools',
+      style: this.styles
+    }, [this.$slots.default])
 
     return h('nav', data, [pad, tools])
   }
